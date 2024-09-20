eff228c5b69a493aa896c96461bc515a14c67f22
Ashish1Velocis <124234068+Ashish1Velocis@users.noreply.github.com>
2024-09-20 12:31:33 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -9,49 +9,12 @@ const App = () => {
     <div className="App">
       <Confetti />
       <header className="App-header">
-        <h1 style={{ marginBottom: "0px" }}>Congratulations!!! Mr. Ashish</h1>
+        <h1 style={{ marginBottom: "0px" }}>Congratulations!!! Mr. Ashish WelcomeTo Again</h1>
         
         <p style={{ marginTop: "10px", marginBottom: "50px" }}>
           You ran your first container with jenkins and sonarqubes.
         </p>
-        <div>
-          <a
-            target="_blank"
-            href={
-              "https://twitter.com/intent/tweet?text=" +
-              shareMessage +
-              "&url=" +
-              shareLink
-            }
-            class="fa-brands fa-x-twitter"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-          <a
-            target="_blank"
-            href={
-              "https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink
-            }
-            class="fa-brands fa-linkedin"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-          <a
-            target="_blank"
-            href={
-              "https://reddit.com/submit?title=" +
-              shareMessage +
-              "&url=" +
-              shareLink
-            }
-            class="fa-brands fa-reddit"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-        </div>
+       
       </header>
     </div>
   );
